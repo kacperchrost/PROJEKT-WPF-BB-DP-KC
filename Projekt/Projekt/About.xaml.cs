98edea518e97a339bb4d4d256c1ec1f2a6e8d775
Kacper <66553710+kacperchrost@users.noreply.github.com>
2022-06-05 21:07:23 +0200

    Dodanie about

---
+++ Projekt/Projekt/About.xaml.cs
@@ -0,0 +1,44 @@
+﻿using HelixToolkit.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Projekt
+{
+    /// <summary>
+    /// Logika interakcji dla klasy About.xaml
+    /// </summary>
+    public partial class About : Page
+    {
+        public About()
+        {
+            InitializeComponent();
+            Create3DViewPort();
+        }
+
+        private void Create3DViewPort() { 
+            var hVp3D = new HelixViewport3D();
+            var lights = new DefaultLights();
+            var teaPot = new Teapot();
+            hVp3D.Children.Add(lights);
+            hVp3D.Children.Add(teaPot);
+            //this.AddChild(hVp3D);
+        }
+
+        private void Back(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("Menu.xaml", UriKind.RelativeOrAbsolute));
+        }
+    }
+}
